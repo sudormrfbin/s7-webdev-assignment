0084345a2a107a56262e3f447c9b87209a21c1f8
Gokul Soumya <gokulps15@gmail.com>
2023-11-17 19:22:24 +0530

    Add freelancer model

---
+++ student-freelance/database/factories/FreelancerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Freelancer>
+ */
+class FreelancerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'username' => $this->faker->userName,
+            'bio' => $this->faker->paragraph,
+        ];
+    }
+}
